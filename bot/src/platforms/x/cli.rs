348ca54bc888ace85668ee0c1a04a69706be2f2c
Denis Nutiu <dnutiu@nuculabs.dev>
2026-04-21 09:23:53 +0300

    fix: X platform cli args not unique

--- bot/src/platforms/x/cli.rs
+++ bot/src/platforms/x/cli.rs
@@ -8,14 +8,14 @@ pub struct XCliArgs {
     pub consumer_key: String,
 
     /// The consumer secret for Oauth1 flow.
-    #[arg(short = 'c', long)]
+    #[arg(short = 's', long)]
     pub consumer_secret: String,
 
     /// The access token.
-    #[arg(short = 'c', long)]
+    #[arg(short = 'a', long)]
     pub access_token: String,
 
     /// The access token secret.
-    #[arg(short = 'c', long)]
+    #[arg(short = 't', long)]
     pub access_token_secret: String,
 }
